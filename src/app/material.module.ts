541d6cb7c1cbb88066267d0a05db2ae248ad451a
cberkane <c.berkane@hotmail.fr>
2022-01-24 20:27:29 +0100

    setup project

---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+
+
+const materialElements = [
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    materialElements
+  ]
+})
+export class MaterialModule { }
