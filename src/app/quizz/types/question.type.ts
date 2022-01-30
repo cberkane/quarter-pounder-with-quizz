9724c909a2047b2456f1f68a5b01848512cd3445
cberkane <c.berkane@hotmail.fr>
2022-01-30 21:07:47 +0100

    added types to safe type data

---
+++ src/app/quizz/types/question.type.ts
@@ -0,0 +1,7 @@
+export interface Question {
+  id: number;
+  questionAsked: string;
+  answer: boolean;
+  actorImgUrl: string;
+  movieImgUrm: string;
+}
---
+++ src/app/quizz/types/score.type.ts
@@ -0,0 +1,5 @@
+export interface Score {
+  id?: number;
+  amount: number;
+  recordedAt: Date;
+}
