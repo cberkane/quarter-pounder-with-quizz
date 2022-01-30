d1269660d465d98b9f4b0a4ba3b3dcc8a4e1e4ea
cberkane <c.berkane@hotmail.fr>
2022-01-30 20:57:23 +0100

    setup material module to share components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 
 const materialElements = [
-  MatButtonModule
+  DragDropModule,
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatIconModule,
+  MatProgressBarModule,
 ];
 
 @NgModule({
