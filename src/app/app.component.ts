d8f5af98b1bd5c06674d1731154ad6eaebd19452
cberkane <c.berkane@hotmail.fr>
2022-01-30 21:43:49 +0100

    updated main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
+import { StoreService } from './shared/services/store.service';
+import { WeclomeComponent } from './shared/components/welcome/welcome.component';
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'quarter-pounder-with-quizz';
+  title = 'The Quarter Pounder With Quizz';
+
+  constructor(
+    private dialog: MatDialog,
+    private store: StoreService
+  ) {}
+
+  ngOnInit(): void {
+    const openedDialog = this.store.getSessionStore('opened-dialog') === 'true';
+    if (openedDialog) {
+      return;
+    }
+    this.openDialog();
+  }
+
+  public openDialog(): void {
+    const options: MatDialogConfig = {
+      width: '450px',
+      height: '350px',
+      data: {
+        message: 'message',
+        path: 'redire',
+      },
+    };
+
+    const dialog = this.dialog.open(WeclomeComponent, options);
+    dialog.afterClosed().subscribe({
+      complete: () => this.store.setSessionStore('opened-dialog', 'true'),
+    });
+  }
 }
